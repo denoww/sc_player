95ac749366a84cb435da35ce0cd597d8c03cf627
diegodev3 <dev3.seucondominio@gmail.com>
2019-06-18 17:38:31 -0300

    alterando a pasta do projeto no raspberry

--- configuracao_tv.sh
+++ configuracao_tv.sh
@@ -5,28 +5,28 @@ echo '--- Configurando variáveis de ambiente'
 read -p 'Informe o ID da TV: ' TV_ID
 echo -e "TV_ID=$TV_ID\n" | sudo tee /etc/environment
 source /etc/environment
-sh -c 'cp ~/workspace/player_tv_raspberry/.env_DEVELOPMENT_sample ~/workspace/player_tv_raspberry/.env_DEVELOPMENT'
+sh -c 'cp ~/player_tv_raspberry/.env_DEVELOPMENT_sample ~/player_tv_raspberry/.env_DEVELOPMENT'
 
 # criando pastas de downloads das midias
 echo '--- Criando pastas de downloads das mídias'
-sh -c 'mkdir ~/workspace/player_tv_raspberry/downloads'
-sh -c 'mkdir ~/workspace/player_tv_raspberry/downloads/videos'
-sh -c 'mkdir ~/workspace/player_tv_raspberry/downloads/images'
-sh -c 'mkdir ~/workspace/player_tv_raspberry/downloads/audios'
-sh -c 'mkdir ~/workspace/player_tv_raspberry/downloads/feeds'
+sh -c 'mkdir ~/player_tv_raspberry/downloads'
+sh -c 'mkdir ~/player_tv_raspberry/downloads/videos'
+sh -c 'mkdir ~/player_tv_raspberry/downloads/images'
+sh -c 'mkdir ~/player_tv_raspberry/downloads/audios'
+sh -c 'mkdir ~/player_tv_raspberry/downloads/feeds'
 
 # configurando barra de tarefas
 echo '--- Configurando barra de tarefas'
-sh -c 'cp ~/workspace/player_tv_raspberry/device_configs/panel /home/pi/.config/lxpanel/LXDE-pi/panels/panel'
+sh -c 'cp ~/player_tv_raspberry/device_configs/panel /home/pi/.config/lxpanel/LXDE-pi/panels/panel'
 
 # criando autostart
 echo '--- Criando autostart'
 sh -c 'mkdir /home/pi/.config/autostart'
-sh -c 'cp ~/workspace/player_tv_raspberry/device_configs/player.desktop /home/pi/.config/autostart/'
+sh -c 'cp ~/player_tv_raspberry/device_configs/player.desktop /home/pi/.config/autostart/'
 
 # configurando wallpaper do dispo
 echo '--- Configurando wallpaper do dispositivo'
-sh -c 'cp ~/workspace/player_tv_raspberry/device_configs/wallpaper.png /home/pi/Pictures/'
+sh -c 'cp ~/player_tv_raspberry/device_configs/wallpaper.png /home/pi/Pictures/'
 sh -c 'pcmanfm --set-wallpaper="/home/pi/Pictures/wallpaper.png"'
 
 # sudo gedit /home/pi/.config/lxsession/LXDE-pi/autostart
