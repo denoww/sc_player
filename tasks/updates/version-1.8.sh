21324ec34d3a66c40a27f90cb375efa5de96044d
diegodev3 <dev3.seucondominio@gmail.com>
2020-07-15 11:37:09 -0300

    melhorando logica do update 1.8

--- tasks/updates/version-1.8.sh
+++ tasks/updates/version-1.8.sh
@@ -2,6 +2,6 @@
 
 cd /home/pi/sc_player/
 rm -rf node_modules/sharp
-npm remove sharp
-npm install sharp
-/usr/bin/npx electron-rebuild
+# npm remove sharp
+# npm install sharp
+# /usr/bin/npx electron-rebuild
