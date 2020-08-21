31a8f642cea36d857d11efd6c057aa5fc590ff5a
diegodev3 <dev3.seucondominio@gmail.com>
2020-08-21 13:17:37 -0300

    melhorando comandos para atualizar para 2.1

--- tasks/updates/version-2.1.sh
+++ tasks/updates/version-2.1.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 export DISPLAY=":0"
 
-cd /home/pi/sc_player/
 # alterando o wallpaper
-sh -c 'cp device_configs/wallpaper.png /home/pi/Pictures/wallpaper.png'
+sh -c 'cp /home/pi/sc_player/device_configs/wallpaper.png /home/pi/Pictures/wallpaper.png'
 sh -c 'pcmanfm --set-wallpaper="/home/pi/Pictures/wallpaper.png"'
 
 # alterando a logo de inicializacao
-sh -c 'sudo cp device_configs/splash.png /usr/share/plymouth/themes/pix/splash.png'
+sh -c 'sudo cp /home/pi/sc_player/device_configs/splash.png /usr/share/plymouth/themes/pix/splash.png'
 
 # adiciona 'logo.nologo' no /boot/cmdline.txt para remover a logo do raspberry
 TEM_LOGO=$(grep -rnw /boot/cmdline.txt -e 'logo.nologo')
